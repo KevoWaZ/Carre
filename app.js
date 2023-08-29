0183770b22a1cf0d38665b73404ce1f21dada5b8
KevoWaZ <kevin.gauthier145@gmail.com>
2023-08-29 21:36:35 +0200

    up

--- app.js
+++ app.js
@@ -3,18 +3,27 @@ let aire = document.getElementById('aire')
 let perimetre = document.getElementById('perimetre')
 let diagonale = document.getElementById('diagonale')
 let btnReset = document.getElementById('reset')
-
+let modal = document.getElementById('modal')
+let Numbers = {
+    arete: arete.value,
+    diagonale: diagonale.value,
+    perimetre: perimetre.value,
+    aire: aire.value
+}
 
 arete.onchange = function () {
     let number = Number(arete.value)
 
     if (number <= 0) {
-        alert("Entrez un nombre positif!!!")
+        arete.classList.replace("is-link", "is-danger")
+        modal.classList.add("is-active")
     } else {
         aire.value = number * number
         perimetre.value = 4 * number
         diagonale.value = Math.sqrt(2) * number
     
+        updateNumbers()
+        dangerToLink()
         areteE()
     }
 }
@@ -24,13 +33,16 @@ aire.onchange = function () {
     let number = Number(aire.value)
 
     if (number <= 0) {
-        alert("Entrez un nombre positif!!!")
+        aire.classList.replace("is-link", "is-danger")
+        modal.classList.add("is-active")
     } else {
         let newAire = Math.sqrt(number)
         arete.value = newAire
         perimetre.value = 4 * arete.value
         diagonale.value = Math.sqrt(2) * arete.value
-    
+
+        updateNumbers()
+        dangerToLink()
         aireE()
     }
 }
@@ -39,13 +51,16 @@ perimetre.onchange = function () {
     let number = Number(perimetre.value)
 
     if (number <= 0) {
-        alert("Entrez un nombre positif!!!")
+        perimetre.classList.replace("is-link", "is-danger")
+        modal.classList.add("is-active")
     } else {
         let newArete = number / 4
         arete.value = newArete
         aire.value = arete.value * arete.value
         diagonale.value = Math.sqrt(2) * arete.value
     
+        updateNumbers()
+        dangerToLink()
         perimetreE()
     }
 }
@@ -54,13 +69,16 @@ diagonale.onchange = function () {
     let number = Number(diagonale.value)
 
     if (number <= 0) {
-        alert("Entrez un nombre positif!!!")
+        diagonale.classList.replace("is-link", "is-danger")
+        modal.classList.add("is-active")
     } else {
         let newArete = number / Math.sqrt(2)
         arete.value = newArete
         aire.value = arete.value * arete.value
         perimetre.value = 4 * arete.value
     
+        updateNumbers()
+        dangerToLink()
         diagonaleE()
     }
 }
@@ -73,4 +91,20 @@ btnReset.onclick = function () {
     diagonale.value = ""
     let ajout = document.querySelector('.equation');
     ajout.innerHTML = ""
+    updateNumbers()
+    dangerToLink()
+}
+
+function dangerToLink() {
+    arete.classList.replace("is-danger", "is-link")
+    perimetre.classList.replace("is-danger", "is-link")
+    aire.classList.replace("is-danger", "is-link")
+    diagonale.classList.replace("is-danger", "is-link")
+}
+
+function updateNumbers() {
+    Numbers.arete = arete.value
+    Numbers.diagonale = diagonale.value
+    Numbers.perimetre = perimetre.value
+    Numbers.aire = aire.value
 }
