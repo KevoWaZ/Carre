d88c54479ff67c84b36e3308d426c020aa9386fd
KevoWaZ <kevin.gauthier145@gmail.com>
2023-08-29 15:36:43 +0200

    verification

--- app.js
+++ app.js
@@ -8,45 +8,61 @@ let btnReset = document.getElementById('reset')
 arete.onchange = function () {
     let number = Number(arete.value)
 
-    aire.value = number * number
-    perimetre.value = 4 * number
-    diagonale.value = Math.sqrt(2) * number
-
-    areteE()
+    if (number <= 0) {
+        alert("Entrez un nombre positif!!!")
+    } else {
+        aire.value = number * number
+        perimetre.value = 4 * number
+        diagonale.value = Math.sqrt(2) * number
+    
+        areteE()
+    }
 }
 
 
 aire.onchange = function () {
     let number = Number(aire.value)
 
-    let newAire = Math.sqrt(number)
-    arete.value = newAire
-    perimetre.value = 4 * arete.value
-    diagonale.value = Math.sqrt(2) * arete.value
-
-    aireE()
+    if (number <= 0) {
+        alert("Entrez un nombre positif!!!")
+    } else {
+        let newAire = Math.sqrt(number)
+        arete.value = newAire
+        perimetre.value = 4 * arete.value
+        diagonale.value = Math.sqrt(2) * arete.value
+    
+        aireE()
+    }
 }
 
 perimetre.onchange = function () {
     let number = Number(perimetre.value)
 
-    let newArete = number / 4
-    arete.value = newArete
-    aire.value = arete.value * arete.value
-    diagonale.value = Math.sqrt(2) * arete.value
-
-    perimetreE()
+    if (number <= 0) {
+        alert("Entrez un nombre positif!!!")
+    } else {
+        let newArete = number / 4
+        arete.value = newArete
+        aire.value = arete.value * arete.value
+        diagonale.value = Math.sqrt(2) * arete.value
+    
+        perimetreE()
+    }
 }
 
 diagonale.onchange = function () {
     let number = Number(diagonale.value)
 
-    let newArete = number / Math.sqrt(2)
-    arete.value = newArete
-    aire.value = arete.value * arete.value
-    perimetre.value = 4 * arete.value
-
-    diagonaleE()
+    if (number <= 0) {
+        alert("Entrez un nombre positif!!!")
+    } else {
+        let newArete = number / Math.sqrt(2)
+        arete.value = newArete
+        aire.value = arete.value * arete.value
+        perimetre.value = 4 * arete.value
+    
+        diagonaleE()
+    }
 }
 
 
